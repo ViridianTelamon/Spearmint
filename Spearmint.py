d59941f24f61547394ac323f87f4f42e0c0933c1
Viridian <96497535+ViridianTelamon@users.noreply.github.com>
2023-02-23 12:36:59 -0500

    Update Spearmint.py

--- Spearmint.py
+++ Spearmint.py
@@ -1,4 +1,4 @@
-"""    
+"""
     Copyright (C) 2023 ViridianTelamon (Viridian)
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
